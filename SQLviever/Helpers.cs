16c86b6d618dd1ae8cf9ba73fdbd18c9bba3137f
Nikolas_lol <nick9100@yandex.ru>
2017-05-28 19:31:23 +0300

    Converting tables to Lists? adding delete from table rows

--- SQLviever/Helpers.cs
+++ SQLviever/Helpers.cs
@@ -13,54 +13,114 @@ namespace SQLviever
 
     }
 
+    public class Sell
+    {
+        public int ID { get; set; }
+        public int COUNT { get; set; }
+        public DateTime DATE_SELL { get; set; }
+        public string CLIENT { get; set; }
+        public object DateDelivery { get; set; }
+    }
+    public class Product
+    {
+        public int ID { get; set; }
+        public object NAME { get; set; }
+        public object PRICE { get; set; }
+        public bool IS_EXIST { get; set; }
+        public object TYPE { get; set; }
+        public bool Z_TYPE { get; set; }
+        public object COUNT { get; set; }
+    }
+    public class Client
+    {
+        public int ID { get; set; }
+        public object NAME { get; set; }
+        public object FAMILYNAME { get; set; }
+        public object ADRESS { get; set; }
+        public object PHONE { get; set; }
+        public bool IS_REGULAR { get; set; }
+    }
+    public class SelProd
+    {
+        public int ID { get; set; }
+        public object SELL_NUMBER { get; set; }
+        public object PRODUCT { get; set; }
+    }
+    public class Type
+    {
+        public int ID { get; set; }
+        public object DESRIPTION { get; set; }
+        public bool Z_TYPE { get; set; }
+    }
     public class Connection
     {
        private NpgsqlConnection connect = new NpgsqlConnection("Server=127.0.0.1;Port=5439;User Id=postgres;Password=candMot8;Database=postgres;");
        
        public Connection()
         {
-            //connect = new NpgsqlConnection("Server=127.0.0.1;Port=5439;User Id=postgres;Password=candMot8;Database=postgres;");
             connect.Open();
         }
 
-        public NpgsqlDataReader getSells()
+        public List<Sell> getSells()
         {
             NpgsqlCommand cmd = new NpgsqlCommand("SELECT \"Sells\".id_sells, \"Sells\".cound, \"Sells\".date_sell, (\"Client\".id_client || ', ' || \"Client\".\"name\" || ' ' || \"Client\".\"familyName\"), \"Sells\".date_delivery " 
                 + "FROM \"Sells\", \"Client\" WHERE \"Sells\".id_client = \"Client\".id_client", connect);
             NpgsqlDataReader dr = cmd.ExecuteReader();
+            List<Sell> sells = new List<Sell>();
+            while (dr.Read())
+            {
+                sells.Add(new Sell { ID = Convert.ToInt32(dr[0]), COUNT = Convert.ToInt32(dr[1]), DATE_SELL = Convert.ToDateTime(dr[2]), CLIENT = Convert.ToString(dr[3]), DateDelivery = dr[4] });
+            }
 
-            return dr;
+            return sells;
         }
 
-        public NpgsqlDataReader getProducts()
+        public List<Product> getProducts()
         {
             NpgsqlCommand cmd = new NpgsqlCommand("select \"Product\".id_product, \"Product\".\"name\",\"Product\".price,\"Product\".\"isExist\",(\"Type\".id_type || '. ' || \"Type\".description), \"Type\".\"type\", \"Product\".count from \"Product\",\"Type\" WHERE \"Product\".id_type = \"Type\".id_type order by id_product", connect);
             NpgsqlDataReader dr = cmd.ExecuteReader();
-
-            return dr;
+            List<Product> products = new List<Product>();
+            while (dr.Read())
+            {
+                products.Add(new Product {ID = (int)(dr[0]), NAME = dr[1], PRICE = dr[2], IS_EXIST=(bool)dr[3], TYPE=dr[4], Z_TYPE=(bool)dr[5],COUNT=dr[6]});
+            }
+            return products;
         }
 
-        public NpgsqlDataReader getClients()
+        public List<Client> getClients()
         {
             NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM \"Client\"", connect);
             NpgsqlDataReader dr = cmd.ExecuteReader();
-
-            return dr;
+            List<Client> clients = new List<Client>();
+            while (dr.Read())
+            {
+                clients.Add(new Client { ID = (int)dr[4], NAME = dr[5], FAMILYNAME = dr[0], IS_REGULAR = (bool)dr[3], ADRESS = dr[1], PHONE = dr[2] });
+            }
+            return clients;
         }
 
-        public NpgsqlDataReader getSelProd()
+        public List<SelProd> getSelProd()
         {
             NpgsqlCommand cmd = new NpgsqlCommand("select \"Sells-Product\".id, \"Sells-Product\".id_sells,(\"Product\".id_product || '. ' || \"Product\".\"name\" ||' '||\"Product\".\"price\") from \"Sells-Product\",\"Product\" WHERE \"Product\".id_product = \"Sells-Product\".id_product order by id", connect);
             NpgsqlDataReader dr = cmd.ExecuteReader();
-
-            return dr;
+            List<SelProd> selprods = new List<SelProd>();
+            while (dr.Read())
+            {
+                selprods.Add(new SelProd { ID = (int)dr[0], SELL_NUMBER = dr[1], PRODUCT = dr[2] });
+            }
+            return selprods;
         }
 
-        public NpgsqlDataReader getType()
+        public List<Type> getType()
         {
             NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM \"Type\"", connect);
             NpgsqlDataReader dr = cmd.ExecuteReader();
-            return dr;
+            List<Type> types = new List<Type>();
+            while (dr.Read())
+            {
+                types.Add(new Type { ID = (int)dr[0], DESRIPTION = dr[1], Z_TYPE = (bool)dr[2] });
+            }
+            return types;
         }
         public void InsertClients(string fName, string adress, string phone, bool regular, string name)
         {
@@ -108,6 +168,31 @@ namespace SQLviever
         {
             string p = ToBaStr(Convert.ToString(date_sell.Date));
             NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO \"Sells\" (date_sell, id_client) values (" + p + "," + client_id + ")", connect);
+            cmd.ExecuteReader();
+        }
+        public void delSells(int id)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("delete from \"Sells\" where id_sells = "+id, connect);
+            cmd.ExecuteReader();
+        }
+        public void delClients(int id)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("delete from \"Client\" where id_client = " + id, connect);
+            cmd.ExecuteReader();
+        }
+        public void delProducts(int id)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("delete from \"Product\" where id_product = " + id, connect);
+            cmd.ExecuteReader();
+        }
+        public void delSelProd(int id)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("delete from \"Sells-Product\" where id = " + id, connect);
+            cmd.ExecuteReader();
+        }
+        public void delType(int id)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("delete from \"Type\" where id_type = " + id, connect);
             cmd.ExecuteReader();
         }
         private string ToBaStr(string x)
--- SQLviever/MainWindow.xaml.cs
+++ SQLviever/MainWindow.xaml.cs
@@ -51,7 +51,7 @@ namespace SQLviever
             Products.Columns[1].Header = "Наименование";
             Products.Columns[2].Header = "Цена";
             Products.Columns[3].Header = "Наличие";
-            Products.Columns[4].Header = "Номер, товар";
+            Products.Columns[4].Header = "Номер типа, товар";
             Products.Columns[5].Header = "Тип товара";
             Products.Columns[6].Header = "Количество на складе";
         }
@@ -61,12 +61,12 @@ namespace SQLviever
             Connection Base = new Connection();
             Clients.ItemsSource = Base.getClients();
             Base.Close();
-            Clients.Columns[0].Header = "Фамилия";
-            Clients.Columns[0].Header = "Адрес";
-            Clients.Columns[0].Header = "Телефон";
-            Clients.Columns[0].Header = "Признак \nпостоянного \nклиента";
-            Clients.Columns[0].Header = "Номер";
-            Clients.Columns[0].Header = "Имя";
+            Clients.Columns[0].Header = "ID";
+            Clients.Columns[1].Header = "Имя";
+            Clients.Columns[2].Header = "Фамилия";
+            Clients.Columns[3].Header = "Адрес";
+            Clients.Columns[4].Header = "Телефон";
+            Clients.Columns[5].Header = "Признак \nпостоянного \nклиента";
         }
 
         private void butSelProd_Click(object sender, RoutedEventArgs e)
@@ -84,6 +84,9 @@ namespace SQLviever
             Connection Base = new Connection();
             Type.ItemsSource = Base.getType();
             Base.Close();
+            Type.Columns[0].Header = "ID";
+            Type.Columns[1].Header = "Описание";
+            Type.Columns[2].Header = "Тип по \nсправке";
         }
 
         private void btn_insert_Click(object sender, RoutedEventArgs e)
@@ -134,8 +137,46 @@ namespace SQLviever
 
         private void butDelSells_Click(object sender, RoutedEventArgs e)
         {
-            DataRowView t = Sells.SelectedValue as DataRowView;
-            //string h = t[0].ToString();
+            Sell t = Sells.SelectedItem as Sell;
+            Connection Base = new Connection();
+            Base.delSells(t.ID);
+            Base.Close();
+            MessageBox.Show("Продажа с номером: " + t.ID +"\nбыл удалён");
+        }
+
+        private void butDelProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Product t = Products.SelectedItem as Product;
+            Connection Base = new Connection();
+            Base.delProducts(t.ID);
+            Base.Close();
+            MessageBox.Show("Товар с номером: " + t.ID + "\nбыл удалён");
+        }
+        private void butDelClient_Click(object sender, RoutedEventArgs e)
+        {
+            Client t = Clients.SelectedItem as Client;
+            Connection Base = new Connection();
+            Base.delClients(t.ID);
+            Base.Close();
+            MessageBox.Show("Клиент с номером: " + t.ID + "\nбыл удалён");
+        }
+
+        private void butDelSelProd_Click(object sender, RoutedEventArgs e)
+        {
+            SelProd t = SelProd.SelectedItem as SelProd;
+            Connection Base = new Connection();
+            Base.delSelProd(t.ID);
+            Base.Close();
+            MessageBox.Show("Отчет с номером: " + t.ID + "\nбыл удалён");
+        }
+
+        private void butDelType_Click(object sender, RoutedEventArgs e)
+        {
+            Type t = Type.SelectedItem as Type;
+            Connection Base = new Connection();
+            Base.delType(t.ID);
+            Base.Close();
+            MessageBox.Show("Тип с номером: " + t.ID + "\nбыл удалён");
         }
     }
 }
